279f72d119da1140eaee764748ae11cecdbd4f4c
Ashok Vemulapalli <ashk406@gmail.com>
2024-05-01 18:34:18 +0100

    Add project files.

---
+++ PaymentsAPI/Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentsAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int  PaymentDetailID { get; set; }
+        
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwner { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = string.Empty;
+
+        //mm/yy
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpiryDate { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(13)")]
+        public string SecurityCode { get; set; } = string.Empty;
+    }
+}
